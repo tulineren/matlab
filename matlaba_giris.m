5d0a840e0b90eaecce7526608a9c4e938849215f
Tülin Eren <erentulin5@gmail.com>
2025-05-24 16:00:24 +0300

    Add files via upload

---
+++ matlaba_giris.m
@@ -0,0 +1,26 @@
+sqrt(12^2-2*5*4-4) %Karakök hesaplama
+sin(pi/2) %pi=180
+tan(pi)
+%Parantezin içine radyan cinsinden yazmalıyız. Yoksa hatalı sonuş verir.
+exp(1) %Üs alma, e üzeri demek
+format short
+exp(1)
+abs(-283)
+log(100) %e tabanlı logaritma
+log(exp(1))
+log10(1000)
+factorial(7)
+2^3/(1/2)+3*(2*(4+1))...
+    -2*(1*4*3/8-4)
+format long
+45859/789
+format short e
+45859/789
+format long e %15 ondalık rakamlı bilimsel gösterim
+45859/789
+round(17/8) %En yakın sayıya yuvarlar.
+ceil(17/8) %Sonsuzluğa yuvarlar.
+floor(17/8) %Eksi sonsuzluğa yuvarlar.
+rem(17,8)
+sign(69)
+fix(17/8) %Kesirli sayıların sadece tam kısmını alır.
